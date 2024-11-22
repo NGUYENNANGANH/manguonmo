48bd17d46ebc662ed56999d61869b9e034cd93ce
NGUYENNANGANH <nguyennanganh199203@gmail.com>
2024-11-22 13:09:05 +0700

    Them file

---
+++ app/models.py
@@ -0,0 +1,22 @@
+# from pydantic import BaseModel, EmailStr
+# from datetime import datetime
+# from typing import Optional
+
+# class Donation(BaseModel):
+#     id: Optional[str]
+#     Bank_Account_Number: str
+#     Bank_Name: str
+#     Campaign_Name: str
+#     Contact_Information: EmailStr
+#     Donation_Amount: int
+#     Donation_Type: str
+#     Name: str
+#     Note: Optional[str]
+#     Quantity_of_Goods: int
+#     Status: str
+#     Type_of_Goods: Optional[str]
+#     Unit: Optional[str]
+#     date: datetime
+
+
+
